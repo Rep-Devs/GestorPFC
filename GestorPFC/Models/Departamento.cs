1bf8a971d89c33660cd45a96debb59470ca9c6e2
Alexxand98 <dennysnob98@gmail.com>
2025-02-25 21:38:10 +0100

    Modelos

---
+++ GestorPFC/Models/Departamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorPFC.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public virtual ICollection<Profesor> Profesores { get; set; }
+    }
+
+}
---
+++ GestorPFC/Models/Profesor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorPFC.Models
+{
+    public class Profesor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+
+        public int DepartamentoId { get; set; }
+        public virtual Departamento Departamento { get; set; }
+
+        public virtual ICollection<Curso> Cursos { get; set; }
+    }
+
+}
